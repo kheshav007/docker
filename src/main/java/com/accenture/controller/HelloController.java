498bc3c4614b1415aa8804d11ef716c56c023738
kheshav007 <kheshav@live.com>
2018-07-11 16:07:24 +0400

    docker project

--- src/main/java/com/accenture/controller/HelloController.java
+++ src/main/java/com/accenture/controller/HelloController.java
@@ -5,8 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/docker")
+@Controller
+@RequestMapping("/docker")
 public class HelloController {
 	
 	@GetMapping("/")
